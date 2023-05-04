49d061de0a8a7f0db90c4b25e8518a3272d97cc2
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2023-05-04 21:09:59 +0200

    Added DOM elements to JS

---
+++ javascript.js
@@ -0,0 +1,29 @@
+/*OPERANDS*/
+const ACBtn = document.getElementById('AC')
+const CBtn = document.getElementById('C')
+const remainderBtn = document.getElementById('remainderBtn')
+const divideBtn = document.getElementById('divideBtn')
+const plusBtn = document.getElementById('plusBtn')
+const minusBtn = document.getElementById('minusBtn')
+const multiplyBtn = document.getElementById('multiplyBtn')
+const equalBtn = document.getElementById('equalBtn') 
+
+/*NUMBERS*/
+const oneBtn = document.getElementById('oneBtn') 
+const twoBtn = document.getElementById('twoBtn') 
+const threeBtn = document.getElementById('threeBtn') 
+const fourBtn = document.getElementById('fourBtn') 
+const fiveBtn = document.getElementById('fiveBtn') 
+const sixBtn = document.getElementById('sixBtn')
+const sevenBtn = document.getElementById('sevenBtn')
+const eightBtn = document.getElementById('eightBtn')
+const zeroBtn = document.getElementById('zeroBtn') 
+
+/*DECIMAL*/
+const dotBtn = document.getElementById('dotBtn')
+
+/*DISPLAY OPERATION*/
+const displayPara = document.getElementById('displayPara')
+
+
+
