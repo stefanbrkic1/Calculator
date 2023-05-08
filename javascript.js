98a7db91ba189c5f285984556e3f2186d141071e
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2023-05-08 18:44:11 +0200

    Added functionality

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-/*OPERANDS*/
+/*OPERATORS*/
 const ACBtn = document.getElementById('AC')
 const CBtn = document.getElementById('C')
 const remainderBtn = document.getElementById('remainderBtn')
@@ -9,7 +9,7 @@ const multiplyBtn = document.getElementById('multiplyBtn')
 const equalBtn = document.getElementById('equalBtn') 
 
 /*NUMBERS*/
-const oneBtn = document.getElementById('oneBtn') 
+const oneBtn = document.querySelector('#oneBtn') 
 const twoBtn = document.getElementById('twoBtn') 
 const threeBtn = document.getElementById('threeBtn') 
 const fourBtn = document.getElementById('fourBtn') 
@@ -17,13 +17,439 @@ const fiveBtn = document.getElementById('fiveBtn')
 const sixBtn = document.getElementById('sixBtn')
 const sevenBtn = document.getElementById('sevenBtn')
 const eightBtn = document.getElementById('eightBtn')
+const nineBtn = document.getElementById('nineBtn')
 const zeroBtn = document.getElementById('zeroBtn') 
 
 /*DECIMAL*/
 const dotBtn = document.getElementById('dotBtn')
 
 /*DISPLAY OPERATION*/
-const displayPara = document.getElementById('displayPara')
+const input1= document.getElementById('input1')
+const displayResult = document.getElementById('displayResult')
+
+resultStatus=false;
+operatorStatus=false;
+let firstOperand;
+let secondOperand;
+let operator;
+let result;
+
+//EQUAL BUTTON, GETTING SECOND OPERAND, OPERATING
+equalBtn.addEventListener('click', () => {
+    resultStatus=true;
+    secondOperand = Number(input1.value.substring(1));
+    operate();
+    displayResult.innerHTML=`=${result}`;
+})
+//
+
+//DOBIJANJE PRVOG OPERANDA NAKON SELEKCIJE OPERATORA
+plusBtn.addEventListener('click', () => {
+    if(input1.value.slice(-1)==='.'){
+
+    }
+    else{
+        firstOperand=parseFloat(input1.value)
+        operator='+';
+        input1.value='+';
+        operatorStatus=true;
+    }
+})
+
+minusBtn.addEventListener('click', () => {
+     if(input1.value.slice(-1)==='.'){
+        
+    }
+    else{
+        firstOperand=parseFloat(input1.value)
+        operator='-';
+        input1.value='-';
+        operatorStatus=true;
+    }
+})
+
+multiplyBtn.addEventListener('click', () => {
+    if(input1.value.slice(-1)==='.'){
+        
+    }
+    else{
+        firstOperand=parseFloat(input1.value)
+        operator='*';
+        input1.value='*';
+        operatorStatus=true;
+    }
+})
+
+divideBtn.addEventListener('click', () => {
+    if(input1.value.slice(-1)==='.'){
+        
+    }
+    else{
+        firstOperand=parseFloat(input1.value)
+        operator='*';
+        input1.value='*';
+        operatorStatus=true;
+    }
+})
+
+remainderBtn.addEventListener('click', () => {
+    if(input1.value.slice(-1)==='.'){
+        
+    }
+    else{
+        firstOperand=parseFloat(input1.value)
+        operator='%';
+        input1.value='%';
+        operatorStatus=true;
+    }
+})
+//
+
+dotBtn.addEventListener('click', () => {
+    if(input1.value.includes('.') || input1.value.trim()===""){
+
+    }
+    else{
+        input1.value+='.';
+    }
+})
+
+oneBtn.addEventListener('click', () => {
+    handleResult();
+    if(operatorStatus===true){
+        if(operator==='+'){
+            input1.value='+1';
+            operatorStatus=false;
+        }
+        else if(operator==='-'){
+            input1.value='-1';
+            operatorStatus=false;
+        }
+        else if(operator==='*'){
+            input1.value='*1';
+            operatorStatus=false;
+        }
+        else if(operator==='/'){
+            input1.value='/1';
+            operatorStatus=false;
+        }
+        else if(operator==='%'){
+            input1.value='%1';
+            operatorStatus=false;
+        }
+    }
+    else{
+        input1.value+=1;
+    }
+})
+
+twoBtn.addEventListener('click', () => {
+    handleResult();
+    if(operatorStatus===true){
+        if(operator==='+'){
+            input1.value='+2';
+            operatorStatus=false;
+        }
+        else if(operator==='-'){
+            input1.value='-2';
+            operatorStatus=false;
+        }
+        else if(operator==='*'){
+            input1.value='*2';
+            operatorStatus=false;
+        }
+        else if(operator==='/'){
+            input1.value='/2';
+            operatorStatus=false;
+        }
+        else if(operator==='%'){
+            input1.value='%2';
+            operatorStatus=false;
+        }
+    }
+    else{
+        input1.value+=2;
+    }
+})
 
 
 
+threeBtn.addEventListener('click', () => {
+    handleResult();
+    if(operatorStatus===true){
+        if(operator==='+'){
+            input1.value='+3';
+            operatorStatus=false;
+        }
+        else if(operator==='-'){
+            input1.value='-3';
+            operatorStatus=false;
+        }
+        else if(operator==='*'){
+            input1.value='*3';
+            operatorStatus=false;
+        }
+        else if(operator==='/'){
+            input1.value='/3';
+            operatorStatus=false;
+        }
+        else if(operator==='%'){
+            input1.value='%3';
+            operatorStatus=false;
+        }
+    }
+    else{
+        input1.value+=3;
+    }
+})
+
+fourBtn.addEventListener('click', () => {
+    handleResult();
+    if(operatorStatus===true){
+        if(operator==='+'){
+            input1.value='+4';
+            operatorStatus=false;
+        }
+        else if(operator==='-'){
+            input1.value='-4';
+            operatorStatus=false;
+        }
+        else if(operator==='*'){
+            input1.value='*4';
+            operatorStatus=false;
+        }
+        else if(operator==='/'){
+            input1.value='/4';
+            operatorStatus=false;
+        }
+        else if(operator==='%'){
+            input1.value='%4';
+            operatorStatus=false;
+        }
+    }
+    else{
+        input1.value+=4;
+    }
+})
+
+fiveBtn.addEventListener('click', () => {
+    handleResult();
+    if(operatorStatus===true){
+        if(operator==='+'){
+            input1.value='+5';
+            operatorStatus=false;
+        }
+        else if(operator==='-'){
+            input1.value='-5';
+            operatorStatus=false;
+        }
+        else if(operator==='*'){
+            input1.value='*5';
+            operatorStatus=false;
+        }
+        else if(operator==='/'){
+            input1.value='/5';
+            operatorStatus=false;
+        }
+        else if(operator==='%'){
+            input1.value='%5';
+            operatorStatus=false;
+        }
+    }
+    else{
+        input1.value+=5;
+    }
+})
+
+sixBtn.addEventListener('click', () => {
+    handleResult();
+    if(operatorStatus===true){
+        if(operator==='+'){
+            input1.value='+6';
+            operatorStatus=false;
+        }
+        else if(operator==='-'){
+            input1.value='-6';
+            operatorStatus=false;
+        }
+        else if(operator==='*'){
+            input1.value='*6';
+            operatorStatus=false;
+        }
+        else if(operator==='/'){
+            input1.value='/6';
+            operatorStatus=false;
+        }
+        else if(operator==='%'){
+            input1.value='%6';
+            operatorStatus=false;
+        }
+    }
+    else{
+        input1.value+=6;
+    }
+})
+
+sevenBtn.addEventListener('click', () => {
+    handleResult();
+    if(operatorStatus===true){
+        if(operator==='+'){
+            input1.value='+7';
+            operatorStatus=false;
+        }
+        else if(operator==='-'){
+            input1.value='-7';
+            operatorStatus=false;
+        }
+        else if(operator==='*'){
+            input1.value='*7';
+            operatorStatus=false;
+        }
+        else if(operator==='/'){
+            input1.value='/7';
+            operatorStatus=false;
+        }
+        else if(operator==='%'){
+            input1.value='%7';
+            operatorStatus=false;
+        }
+    }
+    else{
+        input1.value+=7;
+    }
+})
+
+eightBtn.addEventListener('click', () => {
+    handleResult();
+    if(operatorStatus===true){
+        if(operator==='+'){
+            input1.value='+8';
+            operatorStatus=false;
+        }
+        else if(operator==='-'){
+            input1.value='-8';
+            operatorStatus=false;
+        }
+        else if(operator==='*'){
+            input1.value='*8';
+            operatorStatus=false;
+        }
+        else if(operator==='/'){
+            input1.value='/8';
+            operatorStatus=false;
+        }
+        else if(operator==='%'){
+            input1.value='%8';
+            operatorStatus=false;
+        }
+    }
+    else{
+        input1.value+=8;
+    }
+})
+
+nineBtn.addEventListener('click', () => {
+    handleResult();
+    if(operatorStatus===true){
+        if(operator==='+'){
+            input1.value='+9';
+            operatorStatus=false;
+        }
+        else if(operator==='-'){
+            input1.value='-9';
+            operatorStatus=false;
+        }
+        else if(operator==='*'){
+            input1.value='*9';
+            operatorStatus=false;
+        }
+        else if(operator==='/'){
+            input1.value='/9';
+            operatorStatus=false;
+        }
+        else if(operator==='%'){
+            input1.value='%9';
+            operatorStatus=false;
+        }
+    }
+    else{
+        input1.value+=9;
+    }
+})
+
+zeroBtn.addEventListener('click', () => {
+    handleResult();
+    if(operatorStatus===true){
+        if(operator==='+'){
+            input1.value='+0';
+            operatorStatus=false;
+        }
+        else if(operator==='-'){
+            input1.value='-0';
+            operatorStatus=false;
+        }
+        else if(operator==='*'){
+            input1.value='*0';
+            operatorStatus=false;
+        }
+        else if(operator==='/'){
+            input1.value='/0';
+            operatorStatus=false;
+        }
+        else if(operator==='%'){
+            input1.value='%0';
+            operatorStatus=false;
+        }
+    }
+    else{
+        input1.value+=0;
+    }
+})
+
+
+function operate(){
+    if(operator==='+'){
+        result=firstOperand+secondOperand;
+    }
+    else if(operator==='-'){
+        result=firstOperand-secondOperand;
+    }
+    else if(operator==='*'){
+        result=firstOperand*secondOperand;
+    }
+    else if(operator==='/'){
+        result=firstOperand/secondOperand;
+    }
+    else if(operator==='%'){
+        result=firstOperand%secondOperand;
+    }
+}
+
+ACBtn.addEventListener('click', () => {
+    input1.value='';
+    displayResult.innerHTML='';
+})
+
+CBtn.addEventListener('click', () => {
+    let inputValueAfterC = input1.value.slice(0, -1);
+    input1.value=inputValueAfterC;
+})
+
+
+function handleResult(){
+    if(resultStatus===true){
+        displayResult.innerHTML='';
+        input1.value='';
+        resultStatus=false;
+    }
+}
+
+/*
+function handleOperator(){
+    if(resultStatus===true){
+        input1.value=displayResult.textContent;
+        resultStatus=false;
+        console.log(displayResult.innerHTML);
+    }
+}
+*/
